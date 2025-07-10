4a14a1f04698b4e34a9d38f55dd4aa8c87e6077a
nav380 <naveenchauhan380@gmail.com>
2025-07-10 14:28:07 +0530

    lamd

--- js/slider.js
+++ js/slider.js
@@ -52,9 +52,9 @@ document.addEventListener('DOMContentLoaded', () => {
     // Sticky background image change
     const bg = document.getElementById('sticky-bg');
     const images = [
-        "url('../images/landing1.jpg')",
-        "url('../images/landing2.jpg')",
-        "url('../images/landing3.jpg')",
+        "url('./images/landing1.jpg')",
+        "url('./images/landing2.jpg')",
+        "url('./images/landing3.jpg')",
         
     ];
     let current = 0;
