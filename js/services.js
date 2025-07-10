dde6850fc5477debd8fe8c399cfa2950559a38a3
nav380 <naveenchauhan380@gmail.com>
2025-07-10 16:18:06 +0530

    comment

--- js/services.js
+++ js/services.js
@@ -1,28 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const additionalServices = [
-        { title: "Resume Building & Optimization" },
-        { title: "LinkedIn Profile Makeover" },
-        { title: "Mock Interviews" },
-        { title: "Career Counseling & Guidance" },
-        { title: "Skill Assessment Tests" },
-        { title: "Soft Skills & Communication Training" },
-        { title: "Job Alerts & Placement Notifications" },
-        { title: "Freelancing & Gig Work Guidance" },
-        { title: "Internship Assistance" },
-        { title: "Overseas Placement Assistance" },
-        { title: "Portfolio Website Development" },
-        { title: "Access to Exclusive Job Fairs" }
-    ];
 
-
-    const grid = document.getElementById('services-grid');
-
-    additionalServices.forEach(service => {
-        const card = document.createElement('div');
-        card.className = "bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300 cursor-pointer text-gray-800 font-medium text-lg";
-        card.textContent = service.title;
-        grid.appendChild(card);
-    });
 
 
     //services
@@ -88,4 +65,31 @@ document.addEventListener('DOMContentLoaded', () => {
         servicesGrid.innerHTML += cardHTML;
     });
 
+
+
+    // extra services
+    const additionalServices = [
+        { title: "Resume Building & Optimization" },
+        { title: "LinkedIn Profile Makeover" },
+        { title: "Mock Interviews" },
+        { title: "Career Counseling & Guidance" },
+        { title: "Skill Assessment Tests" },
+        { title: "Soft Skills & Communication Training" },
+        { title: "Job Alerts & Placement Notifications" },
+        { title: "Freelancing & Gig Work Guidance" },
+        { title: "Internship Assistance" },
+        { title: "Overseas Placement Assistance" },
+        { title: "Portfolio Website Development" },
+        { title: "Access to Exclusive Job Fairs" }
+    ];
+
+
+    const grid = document.getElementById('services-grid');
+
+    additionalServices.forEach(service => {
+        const card = document.createElement('div');
+        card.className = "bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300 cursor-pointer text-gray-800 font-medium text-lg";
+        card.textContent = service.title;
+        grid.appendChild(card);
+    });
 });
